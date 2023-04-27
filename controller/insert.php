c651255cfabdf17ce02fbaad712748c03e5e4430
João Pedro Monção <pedrohjoao44@gmail.com>
2023-04-27 13:51:46 -0300

    🐛 FIX: Bug in the cadaster logic

--- controller/insert.php
+++ controller/insert.php
@@ -95,58 +95,58 @@ if(isset($_REQUEST['cadastro'])) {
     $stmt_verifica_email->close();
     $stmt_insere_usuario->close();
     $conn->close();
-} elseif(isset($_REQUEST['add-tarefa'])) {
-    // inclui o arquivo config.php com a variável $conn
-    require_once('../src/config.php');
+}
+// } elseif(isset($_REQUEST['add-tarefa'])) {
+//     // inclui o arquivo config.php com a variável $conn
+//     require_once('../src/config.php');
 
-    // Obtém as informações da sessão do usuário
-    $usuario = $_SESSION['usuario'];
-    $usuario_id = $usuario['id'] ?? '';
-    $categoria_id = $_POST['categoria-seletor'] ?? '';
-    $titulo = $_POST['titulo'] ?? '';
-    $descricao = $_POST['descricao'] ?? '';
-    $data_conclusao = $_POST['data_conclusao'] ?? '';
-    $horario = $_POST['horario'] ?? '';
-    $recorrente = $_POST['recorrente'] ?? '';
-    $dias_semana = $_POST['dias_semana'] ?? '';
-    $concluida = $_POST['concluida'] ?? '';
+//     // Obtém as informações da sessão do usuário
+//     $usuario = $_SESSION['usuario'];
+//     $usuario_id = $usuario['id'] ?? '';
+//     $categoria_id = $_POST['categoria-seletor'] ?? '';
+//     $titulo = $_POST['titulo'] ?? '';
+//     $descricao = $_POST['descricao'] ?? '';
+//     $data_conclusao = $_POST['data_conclusao'] ?? '';
+//     $horario = $_POST['horario'] ?? '';
+//     $recorrente = $_POST['recorrente'] ?? '';
+//     $dias_semana = $_POST['dias_semana'] ?? '';
+//     $concluida = $_POST['concluida'] ?? '';
 
-    // Verifica se há dados faltando
-    if(empty($categoria_id) || empty($titulo) || empty($descricao) || empty($horario) || empty($data_conclusao) || empty($recorrente) || ($recorrente == 1 && empty($dias_semana))) {
-        $_SESSION['error_message'] = "Por favor, preencha todos os campos.";
-        header("Location: ../view/tarefas.php");
-        exit();
-    }
+//     // Verifica se há dados faltando
+//     if(empty($categoria_id) || empty($titulo) || empty($descricao) || empty($horario) || empty($data_conclusao) || empty($recorrente) || ($recorrente == 1 && empty($dias_semana))) {
+//         $_SESSION['error_message'] = "Por favor, preencha todos os campos.";
+//         header("Location: ../view/tarefas.php");
+//         exit();
+//     }
 
-    // Prepara a instrução SQL para inserção
-    if($recorrente === "1") {
-        $sql_insere_tarefa = "INSERT INTO tarefa (id, usuario_id, categoria_id, titulo, descricao, horario, data_conclusao, recorrente, dias_semana, concluida) VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, 0)";
-    } else {
-        $sql_insere_tarefa = "INSERT INTO tarefa (id, usuario_id, categoria_id, titulo, descricao, horario, recorrente, concluida) VALUES (null, ?, ?, ?, ?, ?, ?, ?, 0)";
-    }
+//     // Prepara a instrução SQL para inserção
+//     if($recorrente === "1") {
+//         $sql_insere_tarefa = "INSERT INTO tarefa (id, usuario_id, categoria_id, titulo, descricao, horario, data_conclusao, recorrente, dias_semana, concluida) VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, 0)";
+//     } else {
+//         $sql_insere_tarefa = "INSERT INTO tarefa (id, usuario_id, categoria_id, titulo, descricao, horario, recorrente, concluida) VALUES (null, ?, ?, ?, ?, ?, ?, ?, 0)";
+//     }
 
-    // Prepara a declaração SQL com placeholders (?)
-    $stmt_insere_tarefa = $conn->prepare($sql_insere_tarefa);
+//     // Prepara a declaração SQL com placeholders (?)
+//     $stmt_insere_tarefa = $conn->prepare($sql_insere_tarefa);
 
-    // Vincula os parâmetros aos placeholders
-    if($recorrente === "1") {
-        $stmt_insere_tarefa->bind_param("iissssisi", $usuario_id, $categoria_id, $titulo, $descricao, $data_conclusao, $recorrente, $dias_semana, $concluida);
-    } else {
-        $stmt_insere_tarefa->bind_param("iissssii", $usuario_id, $categoria_id, $titulo, $descricao, $data_conclusao, $recorrente, $concluida);
-    }
+//     // Vincula os parâmetros aos placeholders
+//     if($recorrente === "1") {
+//         $stmt_insere_tarefa->bind_param("iissssisi", $usuario_id, $categoria_id, $titulo, $descricao, $data_conclusao, $recorrente, $dias_semana, $concluida);
+//     } else {
+//         $stmt_insere_tarefa->bind_param("iissssii", $usuario_id, $categoria_id, $titulo, $descricao, $data_conclusao, $recorrente, $concluida);
+//     }
 
-    // Executa a instrução SQL de inserção
-    if ($stmt_insere_tarefa->execute()) {
-        // Redireciona para a página de tarefas
-        header('Location: ../view/tarefas.php');
-        exit();
-    } else {
-        // Exibe uma mensagem de erro caso ocorra algum problema na inserção
-        $_SESSION['error_message'] = "Erro ao cadastrar tarefa: " . $conn->error . ". Tente novamente mais tarde!";
-        header("Location: ../view/tarefas.php");
-        exit();
-    }
+//     // Executa a instrução SQL de inserção
+//     if ($stmt_insere_tarefa->execute()) {
+//         // Redireciona para a página de tarefas
+//         header('Location: ../view/tarefas.php');
+//         exit();
+//     } else {
+//         // Exibe uma mensagem de erro caso ocorra algum problema na inserção
+//         $_SESSION['error_message'] = "Erro ao cadastrar tarefa: " . $conn->error . ". Tente novamente mais tarde!";
+//         header("Location: ../view/tarefas.php");
+//         exit();
+//     }
 
-    // Encerra a conexão com o banco de dados
-    $conn->close();
-?>
+//     // Encerra a conexão com o banco de dados
+//     $conn->close();
